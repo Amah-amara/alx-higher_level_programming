cde1624008577930c77676a89fd932d28a1a4f2a
Amah-amara <cynama99@gmail.com>
2023-03-09 18:07:24 +0100

    task2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """printvthe number of amd list of arguments."""
+    """print the number of and list of arguments."""
     import sys
 
-    count = len(sys.srgv) - 1
+    count = len(sys.argv) - 1
     if count == 0:
         print("0 arguments.")
     elif count == 1:
